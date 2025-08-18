d6b30f4e9ae471537a8d5244dae6a000bdcf0632
leontev alexander <leontevar@vk.com>
2025-08-19 00:43:32 +0300

    *interactive.py bugfixes again and again

--- email_sender_interactive.py
+++ email_sender_interactive.py
@@ -9,8 +9,8 @@ fromsender = input("'From:' field. Place text like alexander@mail.com: ")
 addressee = input("'To:' field. Place text like gigomegoom@mail.com: ")
 subject = input("'Subject:' field. Place any text: ")
 messagebody = input("Message body filed. Place any text: ")
-SMTPserver = input(f"smtp-server dns-name. Place MX-record domain name. If you just hint Enter here  I will use {default_smtpserver}. ") or default_smtpserver
-Port = int(input(f"smtp-server port field. Place port here. If you just hint Enter here  I will use default port {default_smtpport}): ")) or default_smtpport
+SMTPserver = input(f"smtp-server dns-name. Place MX-record domain name. If you just hint Enter here  I will use {default_smtpserver}: ") or default_smtpserver
+Port = int(input(f"smtp-server port field. Place port here. If you just hint Enter here  I will use default port {default_smtpport}: ") or default_smtpport)
 loginsmtp = input("smtp-server login: ")
 passwordsmtp = getpass("smtp-server  password: ")
 
