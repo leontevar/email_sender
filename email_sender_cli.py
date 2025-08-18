79b6c2e4be547459344c616f969c960d3c724f4c
leontev alexander <leontevar@vk.com>
2025-08-19 00:22:25 +0300

    new file email_sender_cli.py - need to test it

---
+++ email_sender_cli.py
@@ -0,0 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from getpass import getpass
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--subject". nargs='?', default="defaultvalue")
+parser.add_argument("-m", "--messagebody". nargs='?', default="defaultvalue")
+parser.add_argument("-f", "--fromsender". nargs='?', default="defaultvalue")
+parser.add_argument("-a", "--addressee". nargs='?', default="defaultvalue")
+parser.add_argument("-S", "--SMTPserver". nargs='?', default="defaultvalue")
+parser.add_argument("-P", "--Port". nargs='?', default="defaultvalue")
+parser.add_argument("-l", "--loginsmtp". nargs='?', default="defaultvalue")
+parser.add_argument("-p", "--passwordsmtp". nargs='?', default="defaultvalue")
+
+
+args = parser.parse_args()
+
+
+
+msg = MIMEText(args.messagebody)
+msg['Subject'] = args.subject
+msg['From'] = args.fromsender
+msg['To'] = args.addressee
+server = smtplib.SMTP(args.SMTPserver, args.Port)
+server.starttls()
+server.login(args.loginsmtp,args.passwordsmtp)
+server.sendmail(args.fromsender, args.addressee, msg.as_string())
+server.quit()
+print("E-mail sent")
