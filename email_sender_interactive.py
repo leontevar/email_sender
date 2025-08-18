e8bec15bbdc1c925169a410b52a3769dab240277
leontev alexander <leontevar@vk.com>
2025-08-18 22:46:44 +0300

    everything is ready to test.

--- email_sender_interactive.py
+++ email_sender_interactive.py
@@ -6,8 +6,19 @@ from email.mime.text import MIMEText
 fromsender = input("'From:' field. Place text like alexander@mail.com: ")
 addressee = input("'To:' field. Place text like gigomegoom@mail.com: ")
 subject = input("'Subject:' field. Place any text: ")
-message = input("Message body filed. Place any text: ")
+messagebody = input("Message body filed. Place any text: ")
 SMTPserver = input("smtp-server dns-name. Place MX-record domain name : ")
-Port = input("smtp-server port field. Place port here (usually is 587): ")
+Port = int(input("smtp-server port field. Place port here (usually is 587): "))
 loginsmtp = input("smtp-server login: ")
 passwordsmtp = input("smtp-server  password: ")
+
+msg = MIMEText(messagebody)
+msg['Subject'] = subject
+msg['From'] = fromsender
+msg['To'] = addressee
+server = smtplib.SMTP(SMTPserver, Port)
+server.starttls()
+server.login(loginsmtp,passwordsmtp)
+server.sendmail(fromsender, addressee, msg.as_string())
+server.quit()
+print("E-mail sent")
